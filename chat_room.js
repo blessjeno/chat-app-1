5845e599c8de03c6f205457f54075182c0c57487
blessjeno <86838748+blessjeno@users.noreply.github.com>
2022-07-10 23:00:45 +0530

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,14 @@
+localStorage.setItem("user_name", user_name);
+
+window.location = "chat_room.html";
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAHaFGJesVe_-Oviram1XdiUDdNfXRsM7c",
+    authDomain: "chat-room-9db15.firebaseapp.com",
+    projectId: "chat-room-9db15",
+    storageBucket: "chat-room-9db15.appspot.com",
+    messagingSenderId: "290938018434",
+    appId: "1:290938018434:web:c1abedc017395806d8329b"
+  };
+ 
+  var app = initializeApp(firebaseConfig);
